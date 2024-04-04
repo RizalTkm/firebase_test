5e3ac824c5eb5a057cdad5af392b37937ea0fb0f
rizal <rzl@accuratelegalbilling.com>
2024-04-04 20:45:26 +0530

    latest push

--- lib/features/product_list/presentation/pages/product_list_screen.dart
+++ lib/features/product_list/presentation/pages/product_list_screen.dart
@@ -1,37 +1,44 @@
+import 'package:firesbase_test/config/routes/routenames.dart';
+import 'package:firesbase_test/features/login/presentation/bloc/auth/cubit/auth_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 // ignore: must_be_immutable
 class ProductListScreen extends StatelessWidget {
-   ProductListScreen({super.key,  this.uid});
+  ProductListScreen({super.key, this.uid});
 
   final String? uid;
 
- var productItems = ['pant','shirt','sari'];
+  var productItems = ['pant', 'shirt', 'sari'];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:const  Text('My App'),
-        // Add any other app bar properties (e.g., actions)
+        title: const Text('My App'),
+        actions: [
+          InkWell(
+              onTap: () {
+                BlocProvider.of<AuthCubit>(context).SignOut().whenComplete(() {
+                  Navigator.of(context).pushNamedAndRemoveUntil(
+                      NavRoutes.loginroute, (route) => false);
+                });
+              },
+              child: const Icon(Icons.logout))
+        ],
       ),
       body: Column(
         children: [
-          // Search Bar (TextField)
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: TextField(
-              onChanged: (value) {
-                // Handle search query changes
-                // Filter cardItems based on the value
-              },
-              decoration: const  InputDecoration(
+              onChanged: (value) {},
+              decoration: const InputDecoration(
                 labelText: 'Search',
                 suffixIcon: Icon(Icons.search),
               ),
             ),
           ),
-          // List of Cards
           Expanded(
             child: ListView.builder(
               itemCount: productItems.length,
@@ -39,11 +46,11 @@ class ProductListScreen extends StatelessWidget {
                 return Card(
                   elevation: 4,
                   child: ListTile(
-                    leading:const  CircleAvatar(
-                      backgroundImage: NetworkImage('https://picsum.photos/200/300'),
+                    leading: const CircleAvatar(
+                      backgroundImage:
+                          NetworkImage('https://picsum.photos/200/300'),
                     ),
                     title: Text(productItems[index]),
-                    // Add other card content (e.g., subtitle, buttons)
                   ),
                 );
               },
