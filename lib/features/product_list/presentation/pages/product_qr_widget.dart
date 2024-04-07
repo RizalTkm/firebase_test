60fac4242ed9b220a7ca20a3783af77c38bcbb5e
rizal <rzl@accuratelegalbilling.com>
2024-04-07 17:30:28 +0530

    latest push

--- lib/features/product_list/presentation/pages/product_qr_widget.dart
+++ lib/features/product_list/presentation/pages/product_qr_widget.dart
@@ -1,3 +1,4 @@
+import 'package:firesbase_test/features/product_list/presentation/widgets/custom_appbar.dart';
 import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 
@@ -9,18 +10,29 @@ class ProductQrWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: const PreferredSize(preferredSize: Size.fromHeight(50), child: CustomappbarWidget(title: "QR code")),
       body: Center(
-        child: QrImageView(
-          data:
-              "${"Name :" + ' ' + snapshotdata['productname'].toString() + '\n' + "Size :" " " + snapshotdata["measurement"].toString()}\nPrice :"
-                      " " +
-                  snapshotdata["price"].toString() +
-                  "\n" +
-                  "Link :" +
-                  " " +
-                  snapshotdata["imageurls"][0].toString(),
-          version: QrVersions.auto,
-          size: 200,
+        child: Column(
+          children: [
+          const  Padding(
+             padding:  EdgeInsets.all(8.0),
+             child:   Text("Please scan the QR code for Product Detais"),
+           ), 
+            Card(elevation: 10,
+              child: QrImageView(
+                data:
+                    "${"Name :" + ' ' + snapshotdata['productname'].toString() + '\n' + "Size :" " " + snapshotdata["measurement"].toString()}\nPrice :"
+                            " " +
+                        snapshotdata["price"].toString() +
+                        "\n" +
+                        "Link :" +
+                        " " +
+                        snapshotdata["imageurls"][0].toString(),
+                version: QrVersions.auto,
+                size: 200,
+              ),
+            ),
+          ],
         ),
       ),
     );
